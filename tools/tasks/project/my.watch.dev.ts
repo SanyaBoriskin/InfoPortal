182581c3cf5874604bf3a2311ad3da7e11d6d1f3
Alex Shirin <alexei.shirin@gmail.com>
2016-05-26 15:23:47 +0300

    Removed unnecessary tasks

--- tools/tasks/project/my.watch.dev.ts
+++
@@ -1,8 +0,0 @@
-import * as gulp from 'gulp';
-import * as browserSync from 'browser-sync';
-
-import { APP_SRC, TEMP_FILES } from '../../config';
-
-export = () => {
-  return gulp.watch(APP_SRC + '/**', ['build.dev', browserSync.reload]);
-}
